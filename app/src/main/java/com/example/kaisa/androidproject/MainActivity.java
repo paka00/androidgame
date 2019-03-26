3797c45ec0f576da42ca62809342ac8c41f60a30
Ville <t7kavi01@students.oamk.fi>
2019-03-26 12:21:04 +0200

    Added first UI version

--- app/src/main/java/com/example/kaisa/androidproject/MainActivity.java
+++ app/src/main/java/com/example/kaisa/androidproject/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.kaisa.androidproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton = findViewById(R.id.btn_Menu);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getSupportActionBar().hide();
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu menu = new PopupMenu(getApplicationContext(), imageButton);
+                menu.getMenuInflater().inflate(R.menu.dropdown, menu.getMenu());
+                menu.show();
+            }
+        });
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment = new ModifyFigureFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment = new AchievementsFragment();
+                    break;
+            }
+            return selectFragment(fragment);
+
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dropdown, menu);
+        return true;
+    }
+
+    public boolean selectFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+        return true;
     }
 }
