0827eaf98ba65e7aa51b370adee07a61e61e22e3
valerihaataja <valeri.haataja@hotmail.com>
2019-03-27 11:19:06 +0200

    hahmonmuokkaus toimintojen lisaysta

--- app/src/main/java/com/example/kaisa/androidproject/ModifyFigureFragment.java
+++ app/src/main/java/com/example/kaisa/androidproject/ModifyFigureFragment.java
@@ -1,6 +1,7 @@
 package com.example.kaisa.androidproject;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,35 +10,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ModifyFigureFragment extends Fragment {
+public class ModifyFigureFragment extends Fragment implements View.OnClickListener{
+    ImageView imageView,imageView1;
+    Button buttonHat,buttonMale,buttonFemale,buttonCancel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_modify_figure, container, false);
 
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button button = getView().findViewById(R.id.testi_button);
-        Button button2 = getView().findViewById(R.id.peru_button);
-        final ImageView imageView = getView().findViewById(R.id.imageview_hattu);
+        buttonHat = getView().findViewById(R.id.testi_button);
+        buttonHat.setOnClickListener(this);
+        buttonCancel = getView().findViewById(R.id.peru_button);
+        buttonCancel.setOnClickListener(this);
+        buttonMale = getView().findViewById(R.id.button_gender_male);
+        buttonMale.setOnClickListener(this);
+        buttonFemale = getView().findViewById(R.id.button_gender_female);
+        buttonFemale.setOnClickListener(this);
+        imageView = getView().findViewById(R.id.imageview_hattu);
+        imageView1 = getView().findViewById(R.id.imageview_character);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageView.setImageResource(R.drawable.hattu);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageView.setImageResource(0);
-            }
-        });
 
     }
+    @Override
+    public void onClick(View v) {
+        int ID = v.getId();
+        if(ID == R.id.testi_button){
+            imageView.setImageResource(R.drawable.hattu);
+        }
+        if(ID == R.id.peru_button){
+            imageView.setImageResource(0);
+        }
+        if(ID == R.id.button_gender_male){
+            imageView1.setImageResource(R.drawable.character_male);
+        }
+        if(ID == R.id.button_gender_female){
+            imageView1.setImageResource(R.drawable.character_female);
+        }
+    }
+
+
 }
