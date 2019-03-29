2ca8527c095be1baf586c7bc99ca2ca44fb96491
Ville <t7kavi01@students.oamk.fi>
2019-03-29 10:26:30 +0200

    Merge branch 'master' of https://github.com/paka00/androidgame into Ville
    # Conflicts:
    #	app/src/main/java/com/example/kaisa/androidproject/MainActivity.java

--- app/src/main/java/com/example/kaisa/androidproject/MainActivity.java
+++ app/src/main/java/com/example/kaisa/androidproject/MainActivity.java
@@ -26,28 +26,50 @@ import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  {
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
 
     ImageButton imageButton = null;
     ViewPager viewPager = null;
     Fragment fragment = null;
+    HomeFragment homeFragment = null;
+    AchievementsFragment achievementsFragment = null;
+    ModifyFigureFragment modifyFigureFragment = null;
+    JoggingFragment joggingFragment = null;
+    SettingsFragment settingsFragment = null;
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeFragment = new HomeFragment();
+        achievementsFragment = new AchievementsFragment();
+        modifyFigureFragment = new ModifyFigureFragment();
+        joggingFragment = new JoggingFragment();
+        settingsFragment = new SettingsFragment();
         imageButton = findViewById(R.id.btn_Menu);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        viewPager = findViewById(R.id.pager);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         getSupportActionBar().hide();
         Intent stepCounterIntent = new Intent(this, StepCounterService.class);
         startService(stepCounterIntent);
-        Log.v("stepsmain", "oncreate");
-        /*
-        viewPager = findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), 3);
-        viewPager.setAdapter(adapter);
-        */
         //selectFragment(homeFragment);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -56,8 +78,36 @@ public class MainActivity extends AppCompatActivity  {
                 PopupMenu menu = new PopupMenu(getApplicationContext(), imageButton);
                 menu.getMenuInflater().inflate(R.menu.dropdown, menu.getMenu());
                 menu.show();
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.menu_settings) {
+                            return selectFragment(settingsFragment);
+                        }
+                        return false;
+                    }
+                });
             }
         });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                navigation.getMenu().getItem(i).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+
+        });
+        setupViewpager(viewPager);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -68,39 +118,47 @@ public class MainActivity extends AppCompatActivity  {
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    fragment = new HomeFragment();
+                    // fragment = homeFragment;
+                    viewPager.setCurrentItem(0);
                     break;
                 case R.id.navigation_dashboard:
-                    fragment = new ModifyFigureFragment();
+                    //fragment = modifyFigureFragment;
+                    viewPager.setCurrentItem(1);
                     break;
                 case R.id.navigation_notifications:
-                    fragment = new AchievementsFragment();
+                    //fragment = achievementsFragment;
+                    viewPager.setCurrentItem(2);
                     break;
                 case R.id.navigation_something:
-                    fragment = new JoggingFragment();
+                    //fragment = joggingFragment;
+                    viewPager.setCurrentItem(3);
                     break;
             }
-            return selectFragment(fragment);
+            // return selectFragment(fragment);
+            return true;
         }
     };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.dropdown, menu);
+        getMenuInflater().inflate(R.menu.dropdown, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if(item.getItemId() == R.id.menu_settings){
-            fragment = new SettingsFragment();
-            TextView textView = findViewById(R.id.fragment_settings_text);
-            textView.setText("Test");
-        }
-        return selectFragment(fragment);
+    protected void onResume() {
+        super.onResume();
+        Intent stepCounterIntent = new Intent(this, StepCounterService.class);
+        stepCounterIntent.putExtra("reset", false);
+        startService(stepCounterIntent);
+        Log.v("stepsmain", "onresume");
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 
     public boolean selectFragment(Fragment fragment) {
         getSupportFragmentManager()
@@ -109,4 +167,13 @@ public class MainActivity extends AppCompatActivity  {
                 .commit();
         return true;
     }
+
+    public void setupViewpager(ViewPager viewPager) {
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), 4);
+        viewPager.setAdapter(adapter);
+    }
+
 }
+
+
+
