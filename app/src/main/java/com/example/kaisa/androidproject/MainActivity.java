60d0859a8fcce81ef7ea583bf1d4ac0b7959fee2
Ville <t7kavi01@students.oamk.fi>
2019-03-29 10:07:13 +0200

    Swipe-toiminto lisatty

--- app/src/main/java/com/example/kaisa/androidproject/MainActivity.java
+++ app/src/main/java/com/example/kaisa/androidproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kaisa.androidproject;
 
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,33 +16,70 @@ import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     ImageButton imageButton = null;
     ViewPager viewPager = null;
     Fragment fragment = null;
+    HomeFragment homeFragment = null;
+    AchievementsFragment achievementsFragment = null;
+    ModifyFigureFragment modifyFigureFragment = null;
+    JoggingFragment joggingFragment = null;
+    SettingsFragment settingsFragment = null;
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeFragment = new HomeFragment();
+        achievementsFragment = new AchievementsFragment();
+        modifyFigureFragment = new ModifyFigureFragment();
+        joggingFragment = new JoggingFragment();
+        settingsFragment = new SettingsFragment();
         imageButton = findViewById(R.id.btn_Menu);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        viewPager = findViewById(R.id.pager);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         getSupportActionBar().hide();
-        /*
-        viewPager = findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), 3);
-        viewPager.setAdapter(adapter);
-        */
+        //selectFragment(homeFragment);
+
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PopupMenu menu = new PopupMenu(getApplicationContext(), imageButton);
                 menu.getMenuInflater().inflate(R.menu.dropdown, menu.getMenu());
                 menu.show();
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.menu_settings) {
+                            return selectFragment(settingsFragment);
+                        }
+                        return false;
+                    }
+                });
             }
         });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                navigation.getMenu().getItem(i).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+
+        });
+        setupViewpager(viewPager);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -52,39 +90,33 @@ public class MainActivity extends AppCompatActivity  {
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    fragment = new HomeFragment();
+                    // fragment = homeFragment;
+                    viewPager.setCurrentItem(0);
                     break;
                 case R.id.navigation_dashboard:
-                    fragment = new ModifyFigureFragment();
+                    //fragment = modifyFigureFragment;
+                    viewPager.setCurrentItem(1);
                     break;
                 case R.id.navigation_notifications:
-                    fragment = new AchievementsFragment();
+                    //fragment = achievementsFragment;
+                    viewPager.setCurrentItem(2);
                     break;
                 case R.id.navigation_something:
-                    fragment = new JoggingFragment();
+                    //fragment = joggingFragment;
+                    viewPager.setCurrentItem(3);
                     break;
             }
-            return selectFragment(fragment);
+            // return selectFragment(fragment);
+            return true;
         }
     };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.dropdown, menu);
+        getMenuInflater().inflate(R.menu.dropdown, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if(item.getItemId() == R.id.menu_settings){
-            fragment = new SettingsFragment();
-            TextView textView = findViewById(R.id.fragment_settings_text);
-            textView.setText("Test");
-        }
-        return selectFragment(fragment);
-    }
 
     public boolean selectFragment(Fragment fragment) {
         getSupportFragmentManager()
@@ -93,4 +125,10 @@ public class MainActivity extends AppCompatActivity  {
                 .commit();
         return true;
     }
+
+    public void setupViewpager(ViewPager viewPager) {
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), 4);
+        viewPager.setAdapter(adapter);
+    }
+
 }
