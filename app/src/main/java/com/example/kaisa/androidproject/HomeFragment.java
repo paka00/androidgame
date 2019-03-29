46d952cd2a037a9db9b695ecf2610aa9aaab2a36
paka00 <t7paka00@students.oamk.fi>
2019-03-29 12:08:49 +0200

    Added timer for daily task

--- app/src/main/java/com/example/kaisa/androidproject/HomeFragment.java
+++ app/src/main/java/com/example/kaisa/androidproject/HomeFragment.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +19,13 @@ import android.widget.TextView;
 import com.example.kaisa.androidproject.model.DbModel;
 import com.example.kaisa.androidproject.model.User;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class HomeFragment extends Fragment {
 
@@ -25,6 +33,8 @@ public class HomeFragment extends Fragment {
     TextView dailyTask = null;
     TextView dailyTaskTime = null;
     TextView dailyTaskProgress = null;
+    int dailySteps;
+    CountDownTimer countDownTimer= null;
 
 
     @Override
@@ -37,29 +47,99 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        DbModel model = new DbModel(getContext());
+        User user = model.readUserFromDb();
+        dailySteps = user.getDailySteps();
+        dailyTaskProgress = getView().findViewById(R.id.daily_task_progress);
         dailyStepsCheck();
+        startCountdownTimer();
     }
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            dailySteps = intent.getIntExtra("daily_steps_int", 0);
             dailyStepsCheck();
         }
     };
 
     protected void dailyStepsCheck() {
-        DbModel model = new DbModel(getContext());
-        User user = model.readUserFromDb();
-        int dailySteps = user.getDailySteps();
-        dailyTaskProgress = getView().findViewById(R.id.daily_task_progress);
         if (dailySteps < 1000) {
             double percentage = (double)dailySteps / 1000.0 * 100.0;
             DecimalFormat df = new DecimalFormat("####0.0");
             dailyTaskProgress.setText("Current progress: " + df.format(percentage) + " %");
         }
         else {
-            dailyTaskProgress.setText("Task done!");
+            dailyTaskProgress.setText("");
+            dailyTask.setText("Task done!");
         }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        countDownTimer.cancel();
+        getActivity().unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(broadcastReceiver, new IntentFilter("StepCounter"));
+    }
+
+    protected void startCountdownTimer() {
+        Calendar currentCalendar = Calendar.getInstance();
+        currentCalendar.setTimeZone(TimeZone.getDefault());
+        /*int curYear = currentCalendar.get(Calendar.YEAR);
+        int curMonth = currentCalendar.get(Calendar.MONTH);
+        int curDay = currentCalendar.get(Calendar.DAY_OF_MONTH);*/
+        Calendar timerEndCalendar = Calendar.getInstance();
+        timerEndCalendar.setTimeZone(TimeZone.getDefault());
+        timerEndCalendar.add(Calendar.DATE, 1);
+        timerEndCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        timerEndCalendar.set(Calendar.MINUTE, 0);
+        timerEndCalendar.set(Calendar.SECOND, 0);
+        long timerMillis = timerEndCalendar.getTimeInMillis() - currentCalendar.getTimeInMillis();
+        countDownTimer = new CountDownTimer(timerMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long hours = millisUntilFinished / 3600000;
+                long minutes = (millisUntilFinished - (hours * 3600000)) / 60000;
+                long seconds = (millisUntilFinished - (hours * 3600000) - (minutes * 60000)) / 1000;
+                String timeRemaining = formatTime(hours, minutes, seconds);
+                dailyTaskTime = getView().findViewById(R.id.daily_task_time);
+                if (dailySteps < 1000) {
+                    dailyTaskTime.setText("Time remaining: " + timeRemaining);
+                }
+                else {
+                    dailyTaskTime.setText("Time until next task: " + timeRemaining);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                startCountdownTimer();
+            }
+        };
+        countDownTimer.start();
+    }
+
+    protected String formatTime (long hours, long minutes, long seconds) {
+        String hour = "" + hours;
+        String minute = "" + minutes;
+        String second = "" + seconds;
+        if (hours < 10) {
+            hour = "0" + hours;
+        }
+        if (minutes < 10) {
+            minute = "0" + minutes;
+        }
+        if (seconds < 10) {
+            second = "0" + seconds;
+        }
+        String timeRemaining = hour + ":" + minute + ":" + second;
+        return timeRemaining;
     }
 
     public interface OnFragmentInteractionListener {
