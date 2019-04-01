3d3874593320812ae1f09466ce79d5e26e2593b6
joe123 <greenlink2@hotmail.com>
2019-04-01 11:17:45 +0300

    Merge branch 'master' of https://github.com/paka00/androidgame into niko
    # Conflicts:
    #	app/src/main/res/layout/fragment_jogging.xml

--- app/src/main/java/com/example/kaisa/androidproject/JoggingFragment.java
+++ app/src/main/java/com/example/kaisa/androidproject/JoggingFragment.java
@@ -15,6 +15,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
