db5871f5f6028606d2abe4bc541cd0e8d449e167
Ville <t7kavi01@students.oamk.fi>
2019-04-04 18:54:49 +0200

    Started to create "change gender"-feature

--- app/src/main/java/com/example/kaisa/androidproject/ModifyFigureFragment.java
+++ app/src/main/java/com/example/kaisa/androidproject/ModifyFigureFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -22,11 +23,10 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
     ArrayList<Integer> maleHeadList = new ArrayList<Integer>();
     int position = 0;
     int ListMinValue = 0;
-    ImageView imageview_maleHead;
-
-    ImageView imageView, imageView1;
+    ImageView imageview_maleHead, imageview_maleTorso, imageview_maleLegs, imageview_maleFeet;
     MainActivity context;
     public Button buttonHat, buttonMale, buttonFemale, buttonCancel, doneButton;
+    public RadioButton maleButton, femaleButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,6 +39,9 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         imageview_maleHead = getView().findViewById(R.id.imageview_male_head);
+        imageview_maleTorso = getView().findViewById(R.id.imageview_male_torso);
+        imageview_maleLegs = getView().findViewById(R.id.imageview_male_legs);
+        imageview_maleFeet = getView().findViewById(R.id.imageview_male_feet);
         maleHeadList.add(R.drawable.mies_paa_0);
         maleHeadList.add(R.drawable.mies_paa_1);
 
@@ -52,8 +55,6 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
 
         Button doneButton = getView().findViewById(R.id.done_button);
         doneButton.setOnClickListener(this);
-
-
 
 
         getView().setFocusableInTouchMode(true);
@@ -72,30 +73,31 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
         });
         doneButton = getView().findViewById(R.id.done_button);
         doneButton.setOnClickListener(this);
+        maleButton = getView().findViewById(R.id.radio_button_man);
+        femaleButton = getView().findViewById(R.id.radio_button_woman);
     }
 
     @Override
     public void onClick(View v) {
         int buttonID = v.getId();
         if (buttonID == R.id.button_head_to_left) {
-            if (position <= ListMinValue){
-                position = maleHeadList.size()-1;
-            }else {
+            if (position <= ListMinValue) {
+                position = maleHeadList.size() - 1;
+            } else {
                 position--;
             }
             setImage();
         }
         if (buttonID == R.id.button_head_to_right) {
-            if(position >= maleHeadList.size()-1){
+            if (position >= maleHeadList.size() - 1) {
                 position = ListMinValue;
-            }else{
+            } else {
                 position++;
             }
             setImage();
         }
 
-        if(buttonID == R.id.done_button){
-
+        if (buttonID == R.id.done_button) {
             if (context.databaseEmpty) {
                 //If database is empty
                 createNewFigure();
@@ -103,22 +105,45 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
                 Toast.makeText(getActivity(), "Saved!", Toast.LENGTH_SHORT).show();
                 //Tietojentallennus tietokantaan
             }
-
         }
     }
 
 
-    public void setImage(){
-        imageview_maleHead.setImageResource(maleHeadList.get(position));
-    }
+        public void setImage () {
+            imageview_maleHead.setImageResource(maleHeadList.get(position));
+        }
 
 
-    public void createNewFigure() {
-        context.viewPager.disableScroll(false);
-        context.navigation.setVisibility(View.VISIBLE);
-        context.imageButton.setVisibility(View.VISIBLE);
-        context.viewPager.setCurrentItem(0);
-        context.databaseEmpty = false;
-        Toast.makeText(getActivity(), "New figure created!", Toast.LENGTH_SHORT).show();
-    }
+        public void createNewFigure () {
+            context.viewPager.disableScroll(false);
+            context.navigation.setVisibility(View.VISIBLE);
+            context.imageButton.setVisibility(View.VISIBLE);
+            context.viewPager.setCurrentItem(0);
+            context.databaseEmpty = false;
+            Toast.makeText(getActivity(), "New figure created!", Toast.LENGTH_SHORT).show();
+        }
+
+
+/*
+        public void changeGender() {
+            maleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imageview_maleHead.setImageResource(R.drawable.mies_paa_0);
+                    imageview_maleTorso.setImageResource(R.drawable.mies_torso_0);
+                    imageview_maleLegs.setImageResource(R.drawable.mies_alaosa_0);
+                    imageview_maleFeet.setImageResource(R.drawable.mies_jalat_0);
+                }
+            });
+
+            femaleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imageview_maleHead.setImageResource(R.drawable.nainen_paa_0);
+                    imageview_maleTorso.setImageResource(R.drawable.nainen_torso_0);
+                    imageview_maleLegs.setImageResource(R.drawable.nainen_alaosa_0);
+                    imageview_maleFeet.setImageResource(R.drawable.nainen_jalat_0);
+                }
+            });
+        }*/
 }
