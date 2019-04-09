6612d028165e67ba356a1088f47055b35a397369
paka00 <t7paka00@students.oamk.fi>
2019-04-09 08:56:05 +0300

    Pull commit

--- app/src/main/java/com/example/kaisa/androidproject/ModifyFigureFragment.java
+++ app/src/main/java/com/example/kaisa/androidproject/ModifyFigureFragment.java
@@ -81,7 +81,6 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
                 position = maleHeadList.size()-1;
             }else {
                 position--;
-                Toast.makeText(context, "asd", Toast.LENGTH_SHORT).show();
             }
             setImage();
         }
@@ -90,7 +89,6 @@ public class ModifyFigureFragment extends Fragment implements View.OnClickListen
                 position = ListMinValue;
             }else{
                 position++;
-                Toast.makeText(context, "asdasd", Toast.LENGTH_SHORT).show();
             }
             setImage();
         }
