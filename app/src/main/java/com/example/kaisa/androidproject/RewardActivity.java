772d4c5d0246530c42f68f006cb28f58c64a0bcf
Ville <t7kavi01@students.oamk.fi>
2019-04-11 09:25:26 +0300

    test

--- app/src/main/java/com/example/kaisa/androidproject/RewardActivity.java
+++ app/src/main/java/com/example/kaisa/androidproject/RewardActivity.java
@@ -2,10 +2,6 @@ package com.example.kaisa.androidproject;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.kaisa.androidproject.model.DbModel;
 import com.example.kaisa.androidproject.model.User;
@@ -36,7 +31,7 @@ public class RewardActivity extends AppCompatActivity {
         imageButtonTreasure.setImageResource(R.drawable.aarrearkku_kiinni);
         imageViewReward = findViewById(R.id.imageview_vaate);
         guideline = findViewById(R.id.horGuideline1);
-        String[] genders = {"akka", "ukko"};
+        String[] genders = {"ukko", "akka"};
         DbModel model = new DbModel(this);
         User user = model.readUserFromDb();
         int gender = user.getGender();
@@ -45,7 +40,7 @@ public class RewardActivity extends AppCompatActivity {
         int id = intent.getIntExtra("ID", 1);
         randomType = intent.getIntExtra("TYPE", 0);
 
-        resourceString = gender+"_"+types[randomType]+"_"+id;
+        resourceString = genders[gender]+"_"+types[randomType]+"_"+id;
 
         imageButtonTreasure.setOnClickListener(new View.OnClickListener() {
             @Override
