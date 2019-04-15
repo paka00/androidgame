cebd2319f4890155c7bb79108abaae4b6ef342ec
Ville <t7kavi01@students.oamk.fi>
2019-04-15 11:43:52 +0300

    Buttons for joggingfragment, started to do settings

--- app/src/main/java/com/example/kaisa/androidproject/SettingsActivity.java
+++ app/src/main/java/com/example/kaisa/androidproject/SettingsActivity.java
@@ -2,12 +2,38 @@ package com.example.kaisa.androidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        seekBar = findViewById(R.id.seekbar_height);
+        textView = findViewById(R.id.text_view_height);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int height = progress+110;
+                textView.setText("Current height: "+height);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
